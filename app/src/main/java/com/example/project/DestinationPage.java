9c5739beeca8e37f372a9869fe5f9e7c65de5ed7
ValentinG1 <70303476+ValentinG1@users.noreply.github.com>
2022-11-23 18:54:05 -0500

    added project

---
+++ app/src/main/java/com/example/project/DestinationPage.java
@@ -0,0 +1,115 @@
+package com.example.project;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.Resource;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.w3c.dom.Text;
+
+import java.net.URI;
+
+public class DestinationPage extends AppCompatActivity {
+
+    DatabaseReference favDBRef;
+    ImageView backbtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        getSupportActionBar().hide();
+        getWindow().setStatusBarColor(ContextCompat.getColor(DestinationPage.this, R.color.black));
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_destination);
+        String name = getIntent().getStringExtra("Name");
+        String price = getIntent().getStringExtra("Price");
+        String image = String.valueOf(getIntent().getStringExtra("Image"));
+
+
+        TextView nameView = findViewById(R.id.destinationName);
+        TextView priceView = findViewById(R.id.priceView);
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        backbtn = findViewById(R.id.imageViewBack);
+        nameView.setText(name);
+        priceView.setText(price);
+        Glide.with(this).load(image).into(imageView);
+        TextView seeReviews = findViewById(R.id.seeReviewsText);
+        seeReviews.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ReviewsRecycler.class);
+                intent.putExtra("name", name);
+                startActivity(intent);
+            }
+        });
+        TextView addReview = findViewById(R.id.destinationAddReview);
+        addReview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Review.class);
+                intent.putExtra("name", name);
+                startActivity(intent);
+            }
+        });
+
+        backbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(DestinationPage.this, Recycler.class);
+                startActivity(intent);
+            }
+        });
+
+        Button bookButton = findViewById(R.id.button);
+        bookButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Payment.class);
+                startActivity(intent);
+            }
+        });
+
+        TextView addToFavorite = findViewById(R.id.destinationAddToFav);
+        Destination destination = new Destination(name, image, price);
+        String loggedInUserID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        favDBRef = FirebaseDatabase.getInstance().getReference().child("Favorites" + loggedInUserID);
+        addToFavorite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                insertDestinationIntoFavorites(destination, loggedInUserID);
+            }
+        });
+
+
+    }
+    private void insertDestinationIntoFavorites(Destination destination, String loggedUserID) {
+        DatabaseReference tableRef = FirebaseDatabase.getInstance().getReference("Favorites" + loggedUserID);
+        DatabaseReference giftIdRef = tableRef.child("location_name");
+        giftIdRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (!snapshot.exists()) {
+                    favDBRef.child(destination.getLocation_name()).setValue(destination);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/project/Recycler.java
@@ -0,0 +1,118 @@
+package com.example.project;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class Recycler extends AppCompatActivity implements ListenerInterface  { // new
+    RecyclerView recyclerView;
+    private DatabaseReference database;
+    private ArrayList<Destination> destinationsList;
+    private RecyclerAdapter recyclerAdapter;
+    private ImageView backbtn;
+
+
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycle);
+        getSupportActionBar().hide();
+        getWindow().setStatusBarColor(ContextCompat.getColor(Recycler.this, R.color.black));
+        recyclerView = findViewById(R.id.destinations);
+        backbtn = findViewById(R.id.imageButtonRecycle);
+        database = FirebaseDatabase.getInstance().getReference("Locations");
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        destinationsList = new ArrayList<>();
+        recyclerAdapter = new RecyclerAdapter(this, destinationsList, this);
+        recyclerView.setAdapter(recyclerAdapter);
+
+        backbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Recycler.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        database.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    Destination destination = dataSnapshot.getValue(Destination.class);
+                    destinationsList.add(destination);
+
+                }
+                recyclerAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        EditText searchBar = findViewById(R.id.searchBar);
+        searchBar.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+
+            public void afterTextChanged(Editable editable) {
+                filterGiftName(editable.toString());
+            }
+        });
+    }
+    private void filterGiftName(String searchTitle) {
+        ArrayList<Destination> destinationsNamesList = new ArrayList<>();
+        for (Destination destination : destinationsList) {
+            if (destination.getLocation_name().toLowerCase().contains(searchTitle.toLowerCase())) {
+                destinationsNamesList.add(destination);
+            }
+        }
+        recyclerAdapter.filterGiftNameList(destinationsNamesList);
+    }
+
+
+    @Override
+    public void onItemClick(int position) {     // new
+
+        Intent intent = new Intent(Recycler.this, DestinationPage.class);
+        intent.putExtra("Image", destinationsList.get(position).getImageUrl());
+        intent.putExtra("Name", destinationsList.get(position).getLocation_name());
+        intent.putExtra("Price", destinationsList.get(position).getPrice());
+        startActivity(intent);
+        finish();
+    }
+}
