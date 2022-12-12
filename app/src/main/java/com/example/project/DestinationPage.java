ac1024336887a10465f7f73e5d564600dc30e9e9
alexNcomp <alexbuinguyen@gmail.com>
2022-12-11 22:09:40 -0500

    Implemented Stripe API payments

--- app/src/main/java/com/example/project/DestinationPage.java
+++ app/src/main/java/com/example/project/DestinationPage.java
@@ -1,6 +1,7 @@
 package com.example.project;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -75,7 +76,8 @@ public class DestinationPage extends AppCompatActivity {
         bookButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), Payment.class);
+                Uri uri = Uri.parse("https://buy.stripe.com/test_9AQ16K3d69st14Q5kk");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
                 startActivity(intent);
             }
         });
